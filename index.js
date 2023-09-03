75dbadb1154781b257668d99fd75034c5d09fbd7
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 10:16:12 +0600

    get all data

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ async function run() {
     await client.connect();
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
+    app.get('/coffee',async(req,res)=>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post('/coffee',async(req,res)=>{
         const newCoffee = req.body;
         console.log(newCoffee);
